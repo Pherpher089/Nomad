1da44fef9d8ab357cbe7a5b251c3fd90e8b7f4c2
Pherpher089 <christutor089@gmail.com>
2023-10-31 11:41:06 -0700

    built objects now save between plays

--- Assets/_Utils/GenerateObjectId.cs
+++ Assets/_Utils/GenerateObjectId.cs
@@ -5,18 +5,18 @@ public static class GenerateObjectId
     public static string GenerateSourceObjectId(SourceObject so)
     {
         return $"{so.itemIndex}_" +
-                $"{so.transform.localPosition.x.ToString("F2")}_" +
-                $"{so.transform.localPosition.y.ToString("F2")}_" +
-                $"{so.transform.localPosition.z.ToString("F2")}_" +
-                $"{so.transform.localRotation.eulerAngles.y.ToString("F2")}_";
+                $"{so.transform.position.x.ToString("F2")}_" +
+                $"{so.transform.position.y.ToString("F2")}_" +
+                $"{so.transform.position.z.ToString("F2")}_" +
+                $"{so.transform.rotation.eulerAngles.y.ToString("F2")}_";
     }
 
     public static string GenerateItemId(Item item)
     {
         return $"{item.itemIndex}_" +
-                $"{item.transform.localPosition.x.ToString("F2")}_" +
-                $"{item.transform.localPosition.y.ToString("F2")}_" +
-                $"{item.transform.localPosition.z.ToString("F2")}_" +
-                $"{item.transform.localRotation.eulerAngles.y.ToString("F2")}_";
+                $"{item.transform.position.x.ToString("F2")}_" +
+                $"{item.transform.position.y.ToString("F2")}_" +
+                $"{item.transform.position.z.ToString("F2")}_" +
+                $"{item.transform.rotation.eulerAngles.y.ToString("F2")}_";
     }
 }
